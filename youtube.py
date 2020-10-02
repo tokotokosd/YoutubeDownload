a990ff9b3ec30c0927a7c450296705b43cb1b84d
Tornike Archvadze <to2ko@mail.ru>
2020-10-02 05:08:12 +0400

    Final

--- youtube.py
+++ youtube.py
@@ -24,7 +24,7 @@ def get_videos(url):
     options.add_argument("--no-sandbox")
     options.add_argument('log-level=3')
     options.add_argument('--disable-dev-shm-usage')
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument("--user-agent=" + user_agent)
     options.add_argument('window-size=1920,1080')
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
